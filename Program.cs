9e912e8a7948828cab264d3012c4c90c2b07372b
Oleksandr Vietrov <oleksandr.vietrov@gmail.com>
2026-03-17 21:24:10 +0100

    added an alternative output statement

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ class Program
             string[] parts = input.Split(' ');
 
             int[] numbers = Array.ConvertAll(parts, int.Parse);
-
+            Console.WriteLine("Processing numbers");
             Console.WriteLine("Sum: " + StatisticsHelper.CalculateSum(numbers));
 
             Console.WriteLine("Average: " + StatisticsHelper.CalculateAverage(numbers));
